4d96e860ef6711ff6288e402e00bd0e9ddc985d1
Will Quinn <wrdquinn@gmail.com>
2022-09-06 15:50:19 +1200

    added a txt file with code that may or may not come in handy later

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ google.charts.setOnLoadCallback(drawChart);
 
 
 //set initial values for the chart fields, these need to be updated as data is entered.
-
-  const totalSavings = 20;
-  const totalInvestments = 2;
-  const totalExpenses = 3;
+  var totalSavings = 1;
+  var totalInvestments = 1;
+  var totalExpenses = 1;
 //copy/pasted code of google chart.js to initialise a pie chart, I can edit this how I want.
 function drawChart() {
 var data = google.visualization.arrayToDataTable([
@@ -60,6 +59,7 @@ const date = document.querySelector(".date");
 
 //grab the current date using new Date();
 let currentDate = new Date();
+
 //use .getMonth(); on currentDate which is holding the full current date. this will just grab the month number
 const monthsNum = currentDate.getMonth();
 // month = months arrays index which corresponds to the .getmonth value.
@@ -69,47 +69,7 @@ const year = currentDate.getFullYear();
 //we set the text content of the date element with a template literal just containing ${month} ${year} variables.
 date.textContent = `${month} ${year}`
 
-// function setType(){
-//   const option = document.querySelectorAll('option');
-//   if (option.value == "Savings" ){
-//      const savingsNum = document.querySelector('#valueNum');
-//      totalSavings += savingsNum;
-//      console.log(totalSavings);
-//   }else if (option.value == "Investment" ){
-//      const investmentNum = document.querySelector('#valueNum');
-//      totalInvestments += investmentNum;
-//      console.log(totalInvestments);
-//   } else 
-//   option.value == "Expense" 
-//   const ExpenseNum = document.querySelector('#valueNum');
-//   totalExpenses += ExpenseNum;
-//   console.log(totalExpenses);
-// }
-function updateGraph(){
+const day = currentDate.getDay();
 
-  //get all the values from the form
-  //update the values adding them to total const totalSavings totalInvestments totalExpenses
-  //update the new butdget (totalInvestments + totalExpenses - totalSavings) 
-  //update the main div with the new table showing all the entries. append a new one each time form is submitted
-  //save to local storage
-  const form = document.getElementById('form');
-  const type = form.elements['etype'].value;
-  const desc = form.elements['text'];
-  const value= form.elements['valueNum'];
-
-  if(type == "Savings"){
-    const savingsNum = document.getElementById('valueNum');
-    totalSavings += savingsNum.value;
-    alert(totalSavings);
-  }else if (type == "Investment" ){
-    const investmentNum = document.getElementById('valueNum');
-    totalInvestments += investmentNum.value;
-    alert(totalInvestments);
-  } else if (type =="Expense"){
-    const ExpenseNum = document.getElementById('valueNum');
-    totalExpenses += ExpenseNum.value;
-    alert(totalExpenses);
-  }
-}
-   const budget = document.getElementById('budgetNum');
-   budget.innerHTML = totalSavings - totalInvestments - totalExpenses;
+const budget = document.getElementById('budgetNum');
+budget.innerHTML = totalSavings - totalInvestments - totalExpenses;
